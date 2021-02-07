7395b5d9ffa2a81e6edb783670a09ef89668d166
YodasProjects <76970992+YodasProjects@users.noreply.github.com>
2021-02-07 16:37:00 +0300

    Add files via upload

---
+++ discord btc stufffFFF/discord btc stufffFFF/Form1.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Net
+Imports System.Collections.Specialized
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            Dim RawData As String = New WebClient().DownloadString("http://api.alternative.me/v2/ticker/?convert=USD")
+            Dim Result As String = ""
+            If (RawData.Contains(ControlChars.Quote)) Then
+                Dim i As Integer = 0
+                For Each Arg As String In RawData.Split(ControlChars.Quote)
+                    Try
+                        If (Arg.ToLower.Equals("price")) Then
+                            Dim Value As String = RawData.Split(ControlChars.Quote)(i + 1).Trim
+                            If (Value.Contains(".")) Then
+                                Value = Value.Split(".")(0)
+                            End If
+                            Value = Value.Replace(":", "")
+                            Result = Value
+                            Exit For
+                        End If
+                    Catch ex As Exception
+                    End Try
+                    i += 1
+                Next
+                If Not RawData.Equals("") Then
+                    Try
+                        Using webClient As WebClient = New WebClient()
+                            Dim nameValueCollection As NameValueCollection = New NameValueCollection()
+                            nameValueCollection("content") = "1 BTC = $" + Result + " USD"
+                            webClient.Headers("authorization") = TextBox1.Text
+
+                            Dim array3 As Byte() = webClient.UploadValues("https://discord.com/api/v8/channels/" + TextBox2.Text +
+"/messages", nameValueCollection)
+
+                        End Using
+
+                    Finally
+                    End Try
+
+                End If
+            End If
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+End Class
---
+++ discord btc stufffFFF/discord btc stufffFFF/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+' denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+' bu öznitelik değerlerini değiştirin.
+
+' Bütünleştirilmiş kod özniteliklerinin değerlerini gözden geçirin
+
+<Assembly: AssemblyTitle("discord btc stufffFFF")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("discord btc stufffFFF")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Bu proje COM'un kullanımına sunulursa aşağıdaki GUID, typelib kimliği içindir
+<Assembly: Guid("bf8f9900-c9b7-45cc-a017-94aaacb1e798")>
+
+' Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+'
+'      Ana Sürüm
+'      İkincil Sürüm 
+'      Yapı Numarası
+'      Düzeltme
+'
+' Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+' aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
